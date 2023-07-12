08268f619354721f83572452b6e38cc210f52ea5
jikwan <jikwan0327@gmail.com>
2023-07-12 09:12:35 +0900

    fix: 컬쳐면접 수정

--- src/store/progressState.ts
+++ src/store/progressState.ts
@@ -28,7 +28,7 @@ export const useProgressListStore = create<IProgressListState>()(
       { id: 3, name: "라이브코딩", isSelect: false },
       { id: 4, name: "AI 면접", isSelect: false },
       { id: 5, name: "과제 제출", isSelect: false },
-      { id: 6, name: "걸쳐면접", isSelect: false },
+      { id: 6, name: "컬쳐면접", isSelect: false },
       { id: 7, name: "기술면접", isSelect: false },
       { id: 8, name: "최종면접", isSelect: false },
     ],
